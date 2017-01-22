84995c0841fb67bcfc6627a54790365bf2e20ee6
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-01-22 22:16:19 +0100

    Added TLType trait and implementation for basic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate byteorder;
 #[macro_use]
 extern crate error_chain;
 
@@ -16,8 +17,146 @@ mod errors {
     }
 }
 
+use byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
+use std::io::{Write,Read};
+
 use errors::*;
+
+trait TLType : Sized {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>;
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self>;
+}
+
+impl TLType for i32 {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()> {
+        out.write_i32::<LittleEndian>(*self)
+            .chain_err(||ErrorKind::Serialize("i32".into()))
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        input.read_i32::<LittleEndian>()
+            .chain_err(||ErrorKind::Deserialize("i32".into()))
+    }
+}
+impl TLType for i64 {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>{
+        out.write_i64::<LittleEndian>(*self)
+            .chain_err(||ErrorKind::Serialize("i64".into()))
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        input.read_i64::<LittleEndian>()
+            .chain_err(||ErrorKind::Deserialize("i64".into()))
+    }
+}
+impl TLType for f64 {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>{
+        out.write_f64::<LittleEndian>(*self)
+            .chain_err(||ErrorKind::Serialize("f64".into()))
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        input.read_f64::<LittleEndian>()
+            .chain_err(||ErrorKind::Deserialize("f64".into()))
+    }
+}
+// TODO: this is wrong. see doc for how to properly encode length
+impl TLType for Vec<u8> {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>{
+        (self.len() as i32).serialize(out)?;
+        out.write_all(self)
+            .chain_err(||ErrorKind::Serialize("bytes".into()))
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        let size = i32::deserialize(input)? as usize;
+        let mut bytes = vec![0;size];
+        input.read_exact(&mut bytes[..])
+            .chain_err(||ErrorKind::Deserialize("bytes".into()))?;
+        Ok(bytes)
+    }
+}
+impl TLType for String {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>{
+        self.clone().into_bytes().serialize(out)
+            .chain_err(||ErrorKind::Serialize("string".into()))
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        let bytes = TLType::deserialize(input)
+            .chain_err(||ErrorKind::Deserialize("string".into()))?;
+        String::from_utf8(bytes)
+            .chain_err(||ErrorKind::Deserialize("string".into()))
+    }
+}
+impl<T> TLType for Vec<T> where T: TLType {
+    fn serialize<W: Write>(&self,out: &mut W) -> Result<()>{
+        const ID : i32 = 481674261;
+        ID.serialize(out)?;
+        (self.len() as i32).serialize(out)?;
+        for elem in self {
+            elem.serialize(out)?;
+        }
+        Ok(())
+    }
+    fn deserialize<R: Read>(input: &mut R) -> Result<Self> {
+        const ID : i32 = 481674261;
+        let read_id = i32::deserialize(input)?;
+        if ID != read_id {
+            bail!(ErrorKind::Deserialize("vector".into()));
+        }
+        let size = i32::deserialize(input)? as usize;
+        let mut vector = Vec::with_capacity(size);
+        for _ in 0..size {
+            vector.push(T::deserialize(input)?);
+        }
+        Ok(vector)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_i32() {
+        let i = -49302394i32;
+        let mut cur = Cursor::new(Vec::new());
+        i.serialize(&mut cur).unwrap();
+        cur.set_position(0);
+        let ii = i32::deserialize(&mut cur).unwrap();
+        assert_eq!(i,ii);
+    }
+    #[test]
+    fn test_i64() {
+        let i = -493023949029i64;
+        let mut cur = Cursor::new(Vec::new());
+        i.serialize(&mut cur).unwrap();
+        cur.set_position(0);
+        let ii = i64::deserialize(&mut cur).unwrap();
+        assert_eq!(i,ii);
+    }
+    #[test]
+    fn test_f64() {
+        let f = -493023.949029f64;
+        let mut cur = Cursor::new(Vec::new());
+        f.serialize(&mut cur).unwrap();
+        cur.set_position(0);
+        let ff = f64::deserialize(&mut cur).unwrap();
+        assert_eq!(f,ff);
+    }
+    #[test]
+    fn test_bytes() {
+        let b = vec![1u8,2,3,4,5];
+        let mut cur = Cursor::new(Vec::new());
+        b.serialize(&mut cur).unwrap();
+        cur.set_position(0);
+        let bb :Vec<u8> = Vec::deserialize(&mut cur).unwrap();
+        assert_eq!(b,bb);
+    }
+    #[test]
+    fn test_string() {
+        let s = String::from("puppa");
+        let mut cur = Cursor::new(Vec::new());
+        s.serialize(&mut cur).unwrap();
+        cur.set_position(0);
+        let ss  = String::deserialize(&mut cur).unwrap();
+        assert_eq!(s,ss);
+    }
 }
